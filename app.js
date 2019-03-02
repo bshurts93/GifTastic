7b7decf1894140e04c2caf27963dfc49a709a43e
Ben Shurts <bshurts93@gmail.com>
2019-03-02 16:24:29 -0600

    Functioning version of app with 10 gifs appearing per click on default buttons. Next step is new button integration.

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-// Input form to add new button
+// Array of premade buttons
+var buttonItems = [
+  "Peter Griffin",
+  "Lois Griffin",
+  "Stewie Griffin",
+  "Brian Griffin",
+  "Meg Griffin"
+];
+
+var gifLimit = 10;
+
+// Populate button-box with array elements
+for (var i = 0; i < buttonItems.length; i++) {
+  // Create button element
+  newBtn = $("<button>");
+  // Make text match name
+  newBtn.text(buttonItems[i]);
+  // Give class of gif-btn
+  newBtn.attr("class", "gif-btn");
+  // Append to button-box
+  $(".button-box").append(newBtn);
+}
 
 // On button click, populate page with gifs
 $(document).on("click", ".gif-btn", function() {
-  var animal = $(this).attr("data-animal");
+  // Empty gif-box on new click
+  $(".gif-box").empty();
+
+  // Search query string
   var queryURL =
-    "http://api.giphy.com/v1/gifs/search?q=" +
-    animal +
-    "&api_key=eECVxqdhtweCEdB6lK3bCbSMlcNnq0mi&limit=5";
+    "https://api.giphy.com/v1/gifs/search?api_key=eECVxqdhtweCEdB6lK3bCbSMlcNnq0mi&q=" +
+    $(this).text() +
+    "&limit=" +
+    gifLimit +
+    "&offset=0&rating=PG&lang=en";
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
+    // Loop through gifs (based on defined limit)
+    for (var i = 0; i < gifLimit; i++) {
+      var gifURL = response.data[i].images.original.url;
+      var newGif = $("<img>");
+      newGif.attr("src", gifURL);
+      $(".gif-box").append(newGif);
+      $(".gif-box").append("<p>Rating: " + response.data[i].rating + "</p>");
+    }
   });
 });
